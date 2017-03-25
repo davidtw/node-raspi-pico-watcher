0e3dcadaae8b4af9a837ab6215afd55566040373
David Twigger <davidtwigger@gmail.com>
2017-03-25 19:20:57 +0100

    send message

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const pico = require('node-raspi-pico-ups').pico;
+const exec = require('child_process').exec;
+
+const config = require('contacts.json');
+
+const intervalTime = 500;
 
 let currentMode = pico.getCurrentPowerMode();
 console.log('Starting loop');
 
-let interval = setInterval(() => {
-    let newMode = pico.getCurrentPowerMode();
-    if(newMode !== currentMode && newMode === 2) {
-        console.log('Battery mode....');
-    } else if(newMode !== currentMode) {
-        console.log('Power back on...');
+function sendMessageToContacts(messageCode) {
+    let contacts = config['contacts'];
+    if(contacts && contacts.length > 0) {
+        let nbContacts = 0;
+        clearInterval(interval);
+        contacts.forEach((contact) => {
+            if(config['messages'][contact.lang] && config['messages'][contact.lang][messageCode]) {
+                exec('gammu sendsms TEXT ' + contact.number + ' -text "' + config['messages'][contact.lang][messageCode] + '"', (error, stdout, stderr) => {
+                    console.log(stdout);
+                    nbContacts++;
+                    if(nbContacts === contacts.length) {
+                        interval = setInterval(loop, intervalTime);
+                    }
+                });
+            }
+        });
     }
-}, 500);
+}
+
+function loop() {
+    let newMode = pico.getCurrentPowerMode();
+    if (newMode !== currentMode) {
+        if (newMode === 2) {
+            console.log('Battery mode....');
+            sendMessageToContacts('powerDown');
+        } else if (newMode !== currentMode) {
+            console.log('Power back on...');
+            sendMessageToContacts('powerUp');
+        }
+        currentMode = newMode;
+    }
+}
+
+let interval = setInterval(loop, intervalTime);
---
+++ sendSms.sh
@@ -0,0 +1,3 @@
+ #!/bin/sh
+
+gammu sendsms TEXT $3 -text $1

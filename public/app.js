d1dd29ca3f75e9e262d7ee444bfcbcd3533423be
David Twigger <davidtwigger@gmail.com>
2017-04-29 15:46:32 +0200

    add default route

--- public/app.js
+++ public/app.js
@@ -3,8 +3,17 @@
 
     app
         .config(function($stateProvider) {
+            var contactsState = {
+                name: 'contacts',
+                url: '/contacts',
+                templateUrl: 'contacts.html',
+                controller: 'contactsCtrl'
+            };
+
+            $stateProvider.state(contactsState);
+            $stateProvider.otherwise('/contacts');
         })
-        .controller("myCtrl", ['$scope', '$http', function($scope, $http) {
+        .controller("contactsCtrl", ['$scope', '$http', function($scope, $http) {
         $scope.loading = true;
         $http.get('/contacts').then(function (data) {
             $scope.contacts = data.contacts;

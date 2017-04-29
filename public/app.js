7015bc19fc25f44ce67135111f801daa45792431
David Twigger <davidtwigger@gmail.com>
2017-04-29 15:51:31 +0200

    add messages controller

--- public/app.js
+++ public/app.js
@@ -2,22 +2,32 @@
     var app = angular.module("smsApp", ['ui.router']);
 
     app
-        .config(function($stateProvider) {
-            var contactsState = {
+        .config(function($stateProvider, $urlRouterProvider) {
+            let contactsState = {
                 name: 'contacts',
                 url: '/contacts',
                 templateUrl: 'contacts.html',
                 controller: 'contactsCtrl'
             };
+            let messagesState = {
+                name: 'messages',
+                url: '/messages',
+                templateUrl: 'messages.html',
+                controller: 'messagesCtrl'
+            };
 
             $stateProvider.state(contactsState);
-            $stateProvider.otherwise('/contacts');
+            $stateProvider.state(messagesState);
+            $urlRouterProvider.otherwise('/contacts');
         })
         .controller("contactsCtrl", ['$scope', '$http', function($scope, $http) {
-        $scope.loading = true;
-        $http.get('/contacts').then(function (data) {
-            $scope.contacts = data.contacts;
-            $scope.loading = false;
-        })
-    }]);
+            $scope.loading = true;
+            $http.get('/contacts').then(function (data) {
+                $scope.contacts = data.contacts;
+                $scope.loading = false;
+            })
+        }])
+        .controller("messagesCtrl", ['$scope', '$http', function($scope, $http) {
+            
+        }]);
 }());
